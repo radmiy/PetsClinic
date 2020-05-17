cb4745e3e9adfa48895dc243b1515c413bfc85ee
Dzmitry Rabykin <Dzmitry.Rabykin@McKesson.com>
2020-05-17 14:52:55 +0300

    nohttp test poll

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -27,7 +27,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication(proxyBeanMethods = false)
 public class PetClinicApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(PetClinicApplication.class, args);
 	}
+
 }
